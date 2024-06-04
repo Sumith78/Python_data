cc9363dd90e5254ecaa36d88484847e616c0b1a3
Sumith <sumithrajput53361@gmail.com>
2024-06-05 00:05:38 +0530

    added

---
+++ firstprogram.py
@@ -0,0 +1,9 @@
+age=21
+
+if(age>=18): #condition is true
+  print("Can get Licence") #so prints only first statment ignoring the rest of the code even if it is correct
+elif(age==21):
+  print("perfect Match")
+else:
+  print("Not Eligible")
+  
