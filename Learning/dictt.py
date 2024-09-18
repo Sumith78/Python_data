36af4a9a4e2e5c2101bc16e13c1df33bdefcafa7
Sumith kumar singh <sumithrajput53361@gmail.com>
2024-09-18 13:45:06 +0530

    added two files

---
+++ Learning/dictt.py
@@ -0,0 +1,9 @@
+data={
+'first_name':'Asabeneh',
+'last_name':'Yetayeh',
+'country':'Finland', 
+'age':250, 
+'is_married':True,
+'skills':['JS', 'React', 'Node', 'Python']
+}
+print(data)
---
+++ Learning/setss.py
@@ -0,0 +1,4 @@
+data={1,4,3,5}
+print(data)
+
+#order is not important , unordered collection of elements and doesnt not contains duplicate
